89f9f6a3277fe7bc45f709fe1103a9408fe30b98
niganmarin <niganmarin@gmail.comgit config --global user.name niganmaringit config --global user.email niganmarin@gmail.com>
2023-06-13 12:14:44 -0500

    edits

--- Starter_Code/StarterCode/static/js/app.js
+++ Starter_Code/StarterCode/static/js/app.js
@@ -1,129 +1,84 @@
-function loadSamples() {
-    d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json")
-      .then(function(data) {
-        // The JSON data will be available in the 'data' variable
-        console.log(data);
-        // Continue with further processing or visualization
-      })
-      .catch(function(error) {
-        // Handle any error that occurs during fetching the JSON data
-        console.error(error);
-      });
+// Fetch data from a remote source
+var url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
+
+// Load the data using D3 library
+d3.json(url).then(function(data) {
+  
+  
+    // Extract important information from the fetched data
+  var names = data.names; 
+  var metadata = data.metadata; 
+  var samples = data.samples; 
+
+  // Create a dropdown menu for selecting test subjects
+  var dropdownMenu = d3.select("#selDataset"); 
+  dropdownMenu.selectAll("option") 
+    .data(names) 
+    .enter() 
+    .append("option") 
+    .text(function(d) { 
+      return d;
+    });
+
+  // Function to update the charts and metadata based on the selected test subject
+  function updateCharts(selectedSample) {
+
+    // Find the data for the selected test subject
+    var selectedData = samples.find(function(sample) {
+      return sample.id === selectedSample;
+    });
+
+    // Update the bar chart with the top 10 OTUs for the selected test subject
+    var barData = [{
+      x: selectedData.sample_values.slice(0, 10).reverse(), 
+      y: selectedData.otu_ids.slice(0, 10).map(id => `OTU ${id}`).reverse(), 
+      text: selectedData.otu_labels.slice(0, 10).reverse(), 
+      type: "bar", 
+      orientation: "h" 
+    }];
+    Plotly.newPlot("bar", barData); 
+
+    // Update the bubble chart with all OTUs for the selected test subject
+    var bubbleData = [{
+      x: selectedData.otu_ids, 
+      y: selectedData.sample_values, 
+      text: selectedData.otu_labels, 
+      mode: "markers", 
+      marker: {
+        size: selectedData.sample_values, 
+        color: selectedData.otu_ids, 
+        colorscale: "Earth" 
+      }
+    }];
+    Plotly.newPlot("bubble", bubbleData); 
+
+    // Display the metadata for the selected test subject
+    var selectedMetadata = metadata.find(function(sample) {
+      return sample.id === parseInt(selectedSample);
+    });
+    displayMetadata(selectedMetadata);
   }
 
-  loadSamples();
+  // Function to display the metadata for the selected test subject
+  function displayMetadata(metadata) {
+    var metadataPanel = d3.select("#sample-metadata");
+    metadataPanel.html(""); // Clear existing content
 
-  
-  // Load and process the data
-d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json")
-.then(function(data) {
-  // Extract required data for the bar chart
-  var samples = data.samples;
-  var dropdown = d3.select("#selDataset");
-  var chartContainer = d3.select("#bar-chart");
+    // Loop through each metadata key-value pair and display them
+    Object.entries(metadata).forEach(([key, value]) => {
+      metadataPanel.append("p").text(`${key}: ${value}`);
+    });
+  }
 
-  // Populate dropdown menu with sample IDs
-  samples.forEach(function(sample) {
-    dropdown
-      .append("option")
-      .text(sample.id)
-      .property("value", sample.id);
+  // Event listener for dropdown change
+  dropdownMenu.on("change", function() {
+    var selectedSample = d3.select(this).property("value");
+    updateCharts(selectedSample);
   });
 
-  // Call updateChart() when dropdown value changes
-  dropdown.on("change", function() {
-    var selectedSample = dropdown.property("value");
-    updateChart(selectedSample);
-  });
-
-  // Initial chart display
-  var initialSample = dropdown.property("value");
-  updateChart(initialSample);
-})
-.catch(function(error) {
+  // Display initial charts and metadata
+  var initialSample = names[0];
+  updateCharts(initialSample);
+}).catch(function(error) {
   console.error(error);
 });
-
-function updateChart(selectedSample) {
-    d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json")
-      .then(function(data) {
-        var samples = data.samples;
-        var selectedData = samples.find(function(sample) {
-          return sample.id === selectedSample;
-        });
-  
-        // Extract top 10 OTUs data
-        var otuValues = selectedData.sample_values.slice(0, 10).reverse();
-        var otuIds = selectedData.otu_ids.slice(0, 10).map(id => `OTU ${id}`).reverse();
-        var otuLabels = selectedData.otu_labels.slice(0, 10).reverse();
-  
-        // Create the bar chart
-        var trace = {
-          x: otuValues,
-          y: otuIds,
-          text: otuLabels,
-          type: "bar",
-          orientation: "h"
-        };
-  
-        var layout = {
-          title: "Top 10 OTUs",
-          xaxis: { title: "Sample Values" },
-          yaxis: { title: "OTU IDs" }
-        };
-  
-        var chartData = [trace];
-  
-        Plotly.newPlot("bar", chartData, layout);
-      })
-      .catch(function(error) {
-        console.error(error);
-      });
-  }
-  
-
-
-  function updateChart(selectedSample) {
-    d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json")
-      .then(function(data) {
-        var samples = data.samples;
-        var selectedData = samples.find(function(sample) {
-          return sample.id === selectedSample;
-        });
-  
-        // Extract bubble chart data
-        var otuIds = selectedData.otu_ids;
-        var sampleValues = selectedData.sample_values;
-        var markerSizes = selectedData.sample_values;
-        var markerColors = selectedData.otu_ids;
-        var textValues = selectedData.otu_labels;
-  
-        // Create the bubble chart
-        var trace = {
-          x: otuIds,
-          y: sampleValues,
-          text: textValues,
-          mode: "markers",
-          marker: {
-            size: markerSizes,
-            color: markerColors
-          }
-        };
-  
-        var layout = {
-          title: "Bubble Chart",
-          xaxis: { title: "OTU IDs" },
-          yaxis: { title: "Sample Values" }
-        };
-  
-        var chartData = [trace];
-  
-        Plotly.newPlot("bubble", chartData, layout);
-      })
-      .catch(function(error) {
-        console.error(error);
-      });
-  }
-  
-
-  
